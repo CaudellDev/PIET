09ce639d9a08534d66326f6bc5b45585c9886e46
Tyler <caudell.tyler@gmail.com>
2016-02-05 00:38:54 -0600

    Revert "Added MIT License as a stand-alone text file and to the top of each Java file."
    This reverts commit 475d27277c38dd530ecf368936f3c3b958528a07.

--- src/piet/image/translator/PietProgram.java
+++ src/piet/image/translator/PietProgram.java
@@ -1,26 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2016 Tyler.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package piet.image.translator;
 
 import java.awt.Color;
@@ -31,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
@@ -378,31 +354,6 @@ public class PietProgram {
 //        System.out.println("blockCountHelper finished...");
         return result;
     }
-    
-    private ArrayList<PietCodel> blockCountHelper2(PietCodel codel) {
-    ArrayList<PietCodel> accumulator = new ArrayList<>();
-    LinkedList<PietCodel> queue = new LinkedList<>();
-    
-    int col = codel.getCol();
-    int row = codel.getRow();
-    queue.add(codel);
-
-    while (!queue.isEmpty()) {
-            PietCodel ajac = queue.remove();
-            if (ajac != null && codel.equals(ajac.getColor()) && ajac.getValue() != PietCodel.CHECKED ) {
-                accumulator.add(ajac);
-                ajac.setValue(PietCodel.CHECKED);
-                col = ajac.getCol();
-                row = ajac.getRow();
-            }
-            
-            if ( get(col + 1, row) != null ) { queue.addFirst(get(col + 1, row)); }
-            if ( get(col , row + 1) != null ) { queue.addFirst(get(col, row + 1)); }
-            if ( get(col - 1, row) != null ) { queue.addFirst(get(col - 1, row)); }
-            if ( get(col , row - 1) != null ) { queue.addFirst(get(col, row- 1)); }
-    }
-    return accumulator;
-}
     
     private PietCodel[] getCodelBlockCorners(PietCodel codel) {
         ArrayList<PietCodel> border = blockCornerHelper(null, codel);
